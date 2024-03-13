ef6cc49e81de710a47899d7f7c040481027b24a6
Sri Venkat Gurramkonda <143310635+srivenkatyadav17@users.noreply.github.com>
2024-03-13 12:40:47 +0530

    Delete first_python directory

--- first_python/python_basics/area.py
+++
@@ -1,4 +0,0 @@
-base = 10
-height = 5
-area = 1 / 2 * (base * height)
-print('Area of triangle is ', area)
--- first_python/python_basics/if.py
+++
@@ -1,6 +0,0 @@
-num = input('Enter the number: ')
-num = int(num)
-if num % 2 == 0:
-    print("The number is Even")
-else:
-    print("The number is Odd")
